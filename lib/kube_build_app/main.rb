67593492b59ed69c82837be9c60e1f0ea7fc5b61
Martin Mareš <martin.mares@seznam.cz>
2022-09-05 19:29:25 +0200

    Export ENV variable KUBE_BUILD_APP_TIMESTAMP

--- lib/kube_build_app/main.rb
+++ lib/kube_build_app/main.rb
@@ -1,6 +1,5 @@
 module KubeBuildApp
   class Main
-
     attr_reader :args, :apps, :shared_assets
 
     require "erb"
@@ -27,6 +26,8 @@ module KubeBuildApp
       @env = Env.new(@args[:env_name], @args[:target], @args[:summary])
       @apps = Array.new
       @shared_assets = load_shared_assets()
+      # export some interesting values
+      ENV.store("KUBE_BUILD_APP_TIMESTAMP", Time.now.utc.to_s)
     end
 
     def build
@@ -37,7 +38,7 @@ module KubeBuildApp
 
         if @env.summary?
           puts Paint["Table of resources (summary)", :yellow]
-          
+
           rows = []
 
           sum_min_mib = ByteSize.new(0).to_mib
@@ -70,7 +71,7 @@ module KubeBuildApp
               sum_max_cpu_cores += cpu_max_cores
             end
           end
-          table = Terminal::Table.new :headings => ["App name", "Container", "Cpu (min)", "(max)", "Mem (min)", "(max)" ], :rows => rows
+          table = Terminal::Table.new :headings => ["App name", "Container", "Cpu (min)", "(max)", "Mem (min)", "(max)"], :rows => rows
           puts table
           puts Paint["Memory", :yellow]
           puts " => req: #{Paint[sprintf("%10.2f", sum_min_mib), :green]} [MiB]"
@@ -104,7 +105,7 @@ module KubeBuildApp
       str = s.strip.downcase
       if str.end_with? "m"
         c = str[..-2].to_f
-        cpu = c/1000.0
+        cpu = c / 1000.0
       else
         cpu = str.to_f
       end
@@ -138,41 +139,5 @@ module KubeBuildApp
         []
       end
     end
-
   end
 end
-
-=begin
-    def build(what, **opts)
-      src = "#{env.src_dir}/#{what.to_s}"
-      dist = "#{env.target_dir}/#{what.to_s}"
-      puts "Templates from: #{src}"
-      Dir.glob("#{src}/*.#{TEMPLATE_EXTENSION}").select.each do |template|
-        if File.file? template
-          puts " | in:  #{template}"
-          # content = render_template(template)
-
-          if opts[:wrap_as_configmap]
-            file_name = File.basename("#{src}/#{template}", ".*")
-            name = File.basename(file_name, ".*")
-            out_path = "#{dist}/#{name}.#{YAML_EXTENSION}"
-
-            #puts "name: #{name}"
-            #puts "mapespace: #{@env.namespace}"
-            #puts "file_name: #{template}"
-            #puts "out_path: #{out_path}"
-            config_map = KubeBuildApp::ConfigMap.yaml_from_file(name, @env.namespace, template)
-            Utils.mkdir_p(File.dirname(out_path))
-            puts " | out: #{out_path}"
-            File.write(out_path, config_map)
-          end
-
-          #if opts[:wrap_as_configmap]
-          #  safe_and_wrap_template(content)
-          #else
-          #  safe_template(content)
-          #end
-        end
-      end
-    end
-=end
