fa44a1b0f894f329027419bb9d5fc80e46e15d42
Martin Mareš <martin.mares@seznam.cz>
2026-04-23 07:41:32 +0200

    Support encjson API-specific binary selection

--- lib/kube_build_app/encjson.rb
+++ lib/kube_build_app/encjson.rb
@@ -1,15 +1,15 @@
 module KubeBuildApp
   class Encjson
-    ENV['ENCJSON_BIN'] ||= "encjson"
-    ENCJSON_BIN ||= ENV['ENCJSON_BIN']
+    require "shellwords"
 
-    ENV['ENCJSON_KEYDIR'] ||= "#{ENV['HOME']}/.encjson"
-    ENCJSON_KEYDIR ||= ENV['ENCJSON_KEYDIR']
+    ENV["ENCJSON_KEYDIR"] ||= "#{ENV["HOME"]}/.encjson"
 
     EJSON_PUBLIC_KEY_FIELD = "_public_key"
+    API_V1_MARKER = "EncJson[@api=1.0"
+    API_V2_MARKER = "EncJson[@api=2.0"
 
     def self.decrypt_file(file_name)
-      cmd = "#{ENCJSON_BIN} decrypt -k #{ENCJSON_KEYDIR} -f #{file_name}"
+      cmd = "#{Shellwords.escape(encjson_bin_for_file(file_name))} decrypt -k #{Shellwords.escape(encjson_keydir)} -f #{Shellwords.escape(file_name)}"
       content = ""
 
       IO.popen(cmd) { |io|
@@ -28,8 +28,44 @@ module KubeBuildApp
         json[EJSON_PUBLIC_KEY_FIELD] = opts[:public_key]
         File.write(file_name, JSON.pretty_generate(json))
       end
-      cmd = "#{ENCJSON_BIN} decrypt -k #{ENCJSON_KEYDIR} -f #{file_name}"
+      cmd = "#{Shellwords.escape(encjson_bin_for_file(file_name))} encrypt -k #{Shellwords.escape(encjson_keydir)} -f #{Shellwords.escape(file_name)} -w"
       system(cmd)
+    end
+
+    def self.encjson_keydir
+      ENV["ENCJSON_KEYDIR"] || "#{ENV["HOME"]}/.encjson"
+    end
+
+    def self.legacy_bin
+      ENV["ENCJSON_LEGACY_PATH"] || ENV["ENCJSON_LEGACY_BIN"] || ENV["ENCJSON_BIN"] || "encjson"
+    end
+
+    def self.rust_bin
+      ENV["ENCJSON_PATH"] || ENV["ENCJSON_RS_BIN"] || "encjson-rs"
+    end
+
+    def self.encjson_bin_for_file(file_name)
+      case detect_api_version(file_name)
+      when "1.0"
+        legacy_bin
+      when "2.0"
+        rust_bin
+      else
+        # backward-compatible fallback
+        ENV["ENCJSON_BIN"] || legacy_bin
+      end
+    end
+
+    def self.detect_api_version(file_name)
+      return nil unless File.file?(file_name)
+
+      probe = File.read(file_name, 8192)
+      return "2.0" if probe.include?(API_V2_MARKER)
+      return "1.0" if probe.include?(API_V1_MARKER)
+
+      nil
+    rescue StandardError
+      nil
     end
   end
 end
